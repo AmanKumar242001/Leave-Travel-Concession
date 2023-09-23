62b9ee157cb34f3ea88ad16c2031fef8f8cfd898
AmanKumar242001 <kondelaman01@gmail.com>
2023-09-23 15:01:42 +0530

    backend service

--- dep-backend/app.py
+++ dep-backend/app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_session import Session
+from flask_cors import CORS
 import sys
 import collections
 from routes import router
@@ -16,15 +17,17 @@ from functions import sendReminders
 
 if sys.version_info.major == 3 and sys.version_info.minor >= 10:
     from collections.abc import MutableSet
+
     collections.MutableSet = collections.abc.MutableSet
 else:
     from collections import MutableSet
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static/")
+cors = CORS(app, resources={"/*": {"origins": "*"}})
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db.init_app(app)
 
 scheduler = BackgroundScheduler()
@@ -32,8 +35,14 @@ scheduler.start()
 
 
 migrate = Migrate(app, db)
-app.register_blueprint(router)
+app.register_blueprint(router, url_prefix="/api")
 admin = Admin(app)
+
+
+@app.route("/")
+def fronend():
+    return send_from_directory("static", "index.html")
+
 
 @app.route("/home")
 def home():
@@ -41,24 +50,49 @@ def home():
 
 
 class UserView(ModelView):
-    column_list = ('id', 'firstName', 'lastName', 'emailId', 'hometown', 'designation', 'payLevel', 'roleId', 'role', 'dateOfJoining', 'department', 'ltcInfos', 'signUrl')
-    form_columns = ('firstName', 'lastName', 'emailId', 'hometown', 'designation', 'payLevel', 'role', 'dateOfJoining', 'department', 'ltcInfos', 'signUrl')
-    form_overrides = {
-        'signUrl': ImageUploadField
-    }
-    
+    column_list = (
+        "id",
+        "firstName",
+        "lastName",
+        "emailId",
+        "hometown",
+        "designation",
+        "payLevel",
+        "roleId",
+        "role",
+        "dateOfJoining",
+        "department",
+        "ltcInfos",
+        "signUrl",
+    )
+    form_columns = (
+        "firstName",
+        "lastName",
+        "emailId",
+        "hometown",
+        "designation",
+        "payLevel",
+        "role",
+        "dateOfJoining",
+        "department",
+        "ltcInfos",
+        "signUrl",
+    )
+    form_overrides = {"signUrl": ImageUploadField}
+
     form_args = {
-        'signUrl': {
-            'label': 'Image',
-            'base_path': os.path.join(os.path.dirname(__file__), 'uploads'),
-            'url_relative_path': 'uploads'
+        "signUrl": {
+            "label": "Image",
+            "base_path": os.path.join(os.path.dirname(__file__), "uploads"),
+            "url_relative_path": "uploads",
         }
     }
 
 
 class RoleView(ModelView):
-    column_list = ('id', 'roleName', 'stageCurrent', 'nextStage', 'prevStage')
-    form_columns = ('id', 'roleName', 'stageCurrent', 'nextStage', 'prevStage')
+    column_list = ("id", "roleName", "stageCurrent", "nextStage", "prevStage")
+    form_columns = ("id", "roleName", "stageCurrent", "nextStage", "prevStage")
+
 
 admin.add_view(UserView(User, db.session))
 admin.add_view(RoleView(Role, db.session))
@@ -70,5 +104,5 @@ scheduler.add_job(
     trigger=CronTrigger.from_crontab("0 8 * * *"),  # run at midnight every day
 )
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
